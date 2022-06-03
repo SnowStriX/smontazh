fc19a778279e9016146affb9b3b0b742ab00c02d
SnowStriX <secondex@mail.ru>
2022-06-03 13:58:52 +0400

    ---

--- js/index.js
+++ js/index.js
@@ -2,3 +2,12 @@ window.addEventListener("scroll",function(){
     var header = document.querySelector("header");
     header.classList.toggle('sticky',window.scrollY > 0);
 })
+
+var i=0;
+        var image=document.getElementById("bar");
+        // Добавьте свои картинки в массив через запятую
+        var imgs=new Array('bars.svg','Close.svg');
+        function imgsrc() {
+            i++;i%=imgs.length;
+            image.src = imgs[i];
+        }

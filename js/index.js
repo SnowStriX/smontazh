53e2f21e196aba730d9143197255ce4211742893
SnowStriX <secondex@mail.ru>
2022-06-04 12:25:01 +0400

    ---

--- js/index.js
+++ js/index.js
@@ -3,11 +3,46 @@ window.addEventListener("scroll",function(){
     header.classList.toggle('sticky',window.scrollY > 0);
 })
 
-var i=0;
-        var image=document.getElementById("bar");
-        // Добавьте свои картинки в массив через запятую
-        var imgs=new Array('bars.svg','Close.svg');
-        function imgsrc() {
-            i++;i%=imgs.length;
-            image.src = imgs[i];
+$(function() { // jquery
+
+    current_img = [
+        'img/аысысыс1.png',
+        'img/image12.png',
+        'img/image9.png',
+        'img/image10.png',
+        'img/image11.png',
+    ]
+
+    let i = 0;
+
+    function highlight(content){ //выделение индикаторов галлереи
+
+        for(j = 0; j < 5; j++){
+            $('#' + String(j)).removeClass('active');
         }
+
+        content.addClass('active');
+    }
+
+    $("#next").click(function () { // конпка вперед
+        i++;
+        if(i == 5){
+            i = 0;
+        }
+        let conteiner =  $('#' + String(i));
+        $('.mobile_gallery').css('background-image', 'url(' + current_img[i] + ')');
+        highlight(conteiner);
+    });
+    
+    $("#back").click(function () { // кнопка назад
+        i--;
+        if(i < 0){
+            i = 4;
+        }
+        let conteiner =  $('#' + String(i));
+        $('.mobile_gallery').css('background-image', 'url(' + current_img[i] + ')');
+        highlight(conteiner);
+    });
+
+});
+
